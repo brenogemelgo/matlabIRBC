4bfb45c58f5aa8a8ed6ec80c5860005f7f2fe78e
kreeyto <royal.ssamy@gmail.com>
2026-01-29 22:45:16 -0300

    fix: some issues. still need to tweak pressure-based

--- commonFunctions/printMoment.m
+++ commonFunctions/printMoment.m
@@ -1,4 +1,20 @@
-function printMoment(idx, tag, expr19, expr27)
+function printMoment(idx, tag, expr19, expr27, mode)
+
+    if nargin < 5
+        mode = "auto";
+    end
+
+    mode = string(mode);
+
+    if mode == "19"
+        fprintf('       moments[m_i<%d>()] = %s; // %s\n', idx, expression(expr19), tag);
+        return;
+    elseif mode == "27"
+        fprintf('       moments[m_i<%d>()] = %s; // %s\n', idx, expression(expr27), tag);
+        return;
+    elseif mode ~= "auto"
+        error('printMoment: unknown mode "%s". Use "auto", "19", or "27".', mode);
+    end
 
     if exprEqual(expr19, expr27)
         fprintf('   moments[m_i<%d>()] = %s; // %s\n', idx, expression(expr19), tag);
---
+++ pressureBasedTODO/functions/incomingSecondOrder.m
@@ -0,0 +1,35 @@
+function incomingSecondOrder(need)
+
+    if ~(need.mxx || need.myy || need.mzz || need.mxy || need.mxz || need.myz)
+        return;
+    end
+
+    fprintf('\n   // Incoming moments\n');
+
+    if need.mxx
+        fprintf('   const scalar_t mxx_I = velocitySet::calculate_moment<VelocitySet, X, X>(pop, boundaryNormal);\n');
+    end
+
+    if need.myy
+        fprintf('   const scalar_t myy_I = velocitySet::calculate_moment<VelocitySet, Y, Y>(pop, boundaryNormal);\n');
+    end
+
+    if need.mzz
+        fprintf('   const scalar_t mzz_I = velocitySet::calculate_moment<VelocitySet, Z, Z>(pop, boundaryNormal);\n');
+    end
+
+    if need.mxy
+        fprintf('   const scalar_t mxy_I = velocitySet::calculate_moment<VelocitySet, X, Y>(pop, boundaryNormal);\n');
+    end
+
+    if need.mxz
+        fprintf('   const scalar_t mxz_I = velocitySet::calculate_moment<VelocitySet, X, Z>(pop, boundaryNormal);\n');
+    end
+
+    if need.myz
+        fprintf('   const scalar_t myz_I = velocitySet::calculate_moment<VelocitySet, Y, Z>(pop, boundaryNormal);\n');
+    end
+
+    fprintf('\n');
+
+end
